83f7bc5ed6d31f661b928e8ebba326397a5e22a7
RafaelSoaresA <114888968+RafaelSoaresA@users.noreply.github.com>
2024-03-21 20:36:43 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,73 @@
+package estruturaestatica;
+
+import java.util.Stack;
+
+
+public class Teste {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String [] args) {
+		/*Pilha<Integer> pilha = new Pilha<Integer>();
+		
+		for(int i=1; i<=10; i++) {
+			pilha.empilha(i);
+		}
+		
+		System.out.println(pilha);
+		
+		System.out.println(pilha.topo());
+		
+		pilha.empilha(1);
+		pilha.empilha(2);
+		pilha.empilha(3);
+		
+		System.out.println(pilha.desempilha());
+		
+		System.out.println(pilha);
+		*/
+		
+		/*Stack<Integer> stack = new Stack<Integer>();
+		
+		System.out.println(stack.isEmpty());
+		
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		
+		System.out.println(stack.size());
+		
+		System.out.println(stack);
+		
+		System.out.println(stack.peek());
+		
+		System.out.println(stack.pop());
+		
+		System.out.println(stack);*/
+		
+		
+		/*Fila<Integer> fila = new Fila<Integer>();
+		fila.enfileira(1);
+		fila.enfileira(2);
+		fila.enfileira(3);
+		
+		System.out.println("Fila está vazia? " + fila.estaVazia());
+		System.out.println("Tamanho da fila " + fila.tamanho());
+		
+		System.out.println(fila.toString());
+		
+		System.out.println(fila.espiar());
+		
+		if(fila.espiar() == null) {
+			System.out.println("Ninguém na fila");
+		}else {
+			System.out.println("O elemento da primeira posição é " + fila.espiar());
+		}
+		
+		System.out.println(fila.toString());
+		System.out.println(fila.desenfileira());
+		System.out.println(fila.toString());*/
+	
+	}
+}
